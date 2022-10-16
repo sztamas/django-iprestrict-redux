786f58d2215bec9154a5da484c1f0886e324fdfc
Tamas Szabo <szabtam@gmail.com>
2022-10-16 21:27:59 +0300

    Add support for Django 4.x

--- iprestrict/__init__.py
+++ iprestrict/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from .restrictor import IPRestrictor
 
 __all__ = ["IPRestrictor"]
--- iprestrict/management/commands/import_rules.py
+++ iprestrict/management/commands/import_rules.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db import transaction
